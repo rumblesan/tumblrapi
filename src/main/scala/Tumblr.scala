da1950fdaa3184e7c0d8f0b3f21ba89ee624beda
Guy John <guy@rumblesan.com>
2012-09-07 11:40:31 +0100

    Adding Tumblr api classes
    Wrapping scribe a bit to make things neater

---
+++ src/main/scala/Tumblr.scala
@@ -0,0 +1,50 @@
+package com.rumblesan.tumblr.api
+
+import org.scribe.builder._
+import org.scribe.builder.api._
+import org.scribe.model._
+import org.scribe.oauth._
+
+import com.codahale.jerkson.Json._
+
+class TumblrAuthenticate(apiKey:String, apiSecret:String) {
+
+    val service:OAuthService = new ServiceBuilder()
+                                   .provider(classOf[TumblrApi])
+                                   .apiKey(apiKey)
+                                   .apiSecret(apiSecret)
+                                   .build()
+    def getAccessToken() {
+      import java.util.Scanner
+
+      val requestToken = service.getRequestToken()
+
+      println("Go to the following URL to authorize")
+      println(service.getAuthorizationUrl(requestToken))
+      println("Then paste the verifier back here")
+
+      val in       = new Scanner(System.in)
+      val verifier = new Verifier(in.nextLine())
+
+      val accessToken = service.getAccessToken(requestToken, verifier)
+
+      println("Here's the access token")
+      println(accessToken)
+
+    }
+
+}
+
+class TumblrAPI(apiKey:String, apiSecret:String, oauthToken:String, oauthSecret:String) {
+
+  val accessToken = new Token(oauthToken, oauthSecret)
+
+  val service = new ServiceBuilder()
+                    .provider(classOf[TumblrApi])
+                    .apiKey(apiKey)
+                    .apiSecret(apiSecret)
+                    .build()
+
+
+}
+
