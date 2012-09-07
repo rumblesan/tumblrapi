199a83773e7d35354b5536921bb91c84cc794b5d
Guy John <guy@rumblesan.com>
2012-09-07 21:23:42 +0100

    An actual working implementation
    Though I need to figure out the correct way to read the image file

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,6 +1,9 @@
 package com.rumblesan.tumblr.api
 
 import com.codahale.jerkson.Json._
+import java.io.ByteArrayOutputStream
+import java.io.File
+import javax.imageio.ImageIO
 
 object App {
   def main(args: Array[String]) {
@@ -11,9 +14,20 @@ object App {
 
     val cfg = parse[Map[String,String]](lines)
 
+    val image = ImageIO.read(new File("landscape.jpeg"))
+    val baos = new ByteArrayOutputStream()
+    ImageIO.write(image, "jpg", baos)
+    baos.flush()
+    val imageData = baos.toByteArray()
+    baos.close()
+
     val tumblrApi = new TumblrAPI(cfg("apiKey"), cfg("apiSecret"), cfg("oauthToken"), cfg("oauthSecret"))
-    val info = tumblrApi.getInfo("rumblesan.tumblr.com")
-    println("got the info")
+    val info = tumblrApi.apiRequest("followers", "rumblesan.tumblr.com")
     println(info)
+
+    val params = Map("type" -> "photo", "caption" -> "Just testing. Please ignore", "tags" -> "testing")
+    val response = tumblrApi.apiRequest("post", "rumblesan.tumblr.com", "POST", params, imageData)
+    println(response)
+
   }
 }
