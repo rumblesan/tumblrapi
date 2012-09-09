a79a5407eafe34db02e7eb2f24eb7db8acd05d24
Guy John <guy@rumblesan.com>
2012-09-09 10:47:01 +0100

    Adding doc comments to TumblrAPI

--- src/main/scala/Tumblr.scala
+++ src/main/scala/Tumblr.scala
@@ -9,11 +9,23 @@ import com.codahale.jerkson.Json._
 
 import java.util.{UUID, Scanner}
 
+/** This is the TumblrAPI companion object
+  *
+  * It hold methods to simplify adding various parameters to a request or
+  * parsing the reponses from the API
+  */
 object TumblrAPI {
   val apiBase    = "http://api.tumblr.com"
   val apiVersion = "v2"
   val apiUrl     = "%s/%s".format(apiBase, apiVersion)
 
+  /** Easily adds query parameters to an OAuthRequest class
+    *
+    * @param request The OAuthRequest object the params are to be added to
+    * @param params A ''Map'' of ''String'' keys and values to add as query
+    *               paramaters to the request
+    * @return This method is entirely side effects and so returns Unit
+    */
   def addQueryParams(request:OAuthRequest, params:Map[String,String]):Unit = {
     params.foldLeft(request)(
       (request, keyVal) => {
@@ -23,6 +35,13 @@ object TumblrAPI {
     )
   }
 
+  /** Easily adds body parameters to an OAuthRequest class
+    *
+    * @param request The ''OAuthRequest'' object the params are to be added to
+    * @param params A ''Map'' of ''String'' keys and values to add as body
+    *               paramaters to the request
+    * @return This method is entirely side effects and so returns ''Unit''
+    */
   def addBodyParams(request:OAuthRequest, params:Map[String,String]):Unit = {
     params.foldLeft(request)(
       (request, keyVal) => {
@@ -32,6 +51,18 @@ object TumblrAPI {
     )
   }
 
+  /** Adds Multi-Part Form data to the request
+    *
+    * This is used primarily for file uploads. In this situation Tumblr
+    * expects the file data to be attached to the request as form data
+    * with the field name '''data'''. The rest of the paramaters must also
+    * be included as form data, but not any OAuth parameters.
+    *
+    * @param request The ''OAuthRequest'' object to add the data to
+    * @param params A Map of String keys and values for creating the form data
+    * @param fileData The binary file data to be included
+    * @return This method is entirely side effects and so returns ''Unit''
+    */
   def addMultiPartFormParams(request:OAuthRequest,
                              params:Map[String,String],
                              fileData:Array[Byte]):Unit = {
@@ -70,10 +101,24 @@ object TumblrAPI {
     request.addHeader("Content-Length", bodyData.length.toString)
   }
 
+  /** Generates a random boundary string to use with the form data
+    * @return The boundary string
+    */
   def generateBoundaryString():String = {
     UUID.randomUUID.toString
   }
 
+  /** Parses the json data response from a GET request and attempts to
+    * fit it into a suitable case class
+    *
+    * @param jsonData The json string returned from the API call
+    * @param postType The type of post the request was for. This is used
+    *                 when parsing the repsonse into specific case classes
+    * @return The response will be an ''Option'' containing a case class that
+    *         extends the ''TumblrApiResponse'' abstract class.
+    *         If the result status from the API is not '''200''' then
+    *         ''None'' will be returned
+    */
   def parseApiGetResponse(jsonData:String, postType:String = ""):Option[TumblrApiResponse] = {
     val firstParse = parse[TumblrResponse](jsonData)
     firstParse.meta.status match {
@@ -89,6 +134,15 @@ object TumblrAPI {
     }
   }
 
+  /** Parses the json data response from a POST request and attempts to
+    * fit it into a suitable case class
+    *
+    * @param jsonData The json string returned from the API call
+    * @return The response will be an ''Option'' containing a
+    *         ''TumblrPostResponse'' case class.
+    *         If the result status from the API is not '''200''' then
+    *         ''None'' will be returned
+    */
   def parseApiPostResponse(jsonData:String):Option[TumblrApiResponse] = {
     val firstParse = parse[TumblrResponse](jsonData)
     firstParse.meta.status match {
@@ -105,31 +159,85 @@ object TumblrAPI {
 
 }
 
+/** The main TumblrAPI class
+  *
+  * This is essentially just a wrapper around the '''Scribe''' library
+  * and is meant to contain all the logic and parsing necessary to interact
+  * with the Tumblr API.
+  *
+  * @param apiKey the api key used for OAuth
+  * @param apiSecret the api secret used for OAuth
+  * @param oauthToken the token part of the OAuth access token
+  * @param oauthSecret the secret part of the OAuth access token
+  */
 class TumblrAPI(apiKey:String, apiSecret:String, oauthToken:String, oauthSecret:String) {
 
+  /** The OAuth access token used to sign requests
+    *
+    * needs the oauthToken and oauthSecret strings
+    */
   val accessToken = new Token(oauthToken, oauthSecret)
 
+  /** The OAuth service object
+    *
+    * Contains all the OAuth specific behaivour
+    */
   val service = new ServiceBuilder()
                     .provider(classOf[TumblrApi])
                     .apiKey(apiKey)
                     .apiSecret(apiSecret)
                     .build()
 
+  /** The default parameters to add to every request
+    */
   val defaultParams = Map("api_key" -> apiKey)
 
+  /** Generates an OAuth request token from the OAuth service object
+    *
+    * This is used when first authenticating the the OAuth API
+    *
+    * @return The oauth ''Token'' object
+    */
   def getRequestToken():Token = {
     service.getRequestToken()
   }
 
+  /** Generates an Authorization URL for the given request token
+    *
+    * @param requestToken The request token to generate an auth URL for
+    * @return The auth URL to go to
+    */
   def getAuthorizationUrl(requestToken:Token):String = {
     service.getAuthorizationUrl(requestToken)
   }
 
+  /** Generates an OAuth access token from the given request token and 
+    * verification string
+    *
+    * @param requestToken The request token used to generate the auth URL
+    * @param verificationString The verification string generated by Tumblr
+    * @return the OAuth access token
+    */
   def getAccessToken(requestToken:Token, verifcationString:String):Token = {
       val verifier = new Verifier(verifcationString)
       service.getAccessToken(requestToken, verifier)
   }
 
+  /** This is the method that actually creates the OAuth request and sends it
+    *
+    * @todo This could probably do with being more robust. These a bunch of
+    *       Exceptions I don't think I check for.
+    * @param endpoint the API endpoint to query
+    * @param blogUrl the URL of the blog to get information from. If this is
+    *                a blank string then it's assumed to be your own blog
+    * @param method The HTTP method used for the API call.
+    *               Supports '''GET''' or '''POST'''
+    * @param params a ''Map'' of ''Strings'' containing any extra parameters
+    *               to use with the query
+    * @param fileData the binary data of the file to be uploaded, if any
+    * @return an ''Option'' class containing a ''String'' if the request
+    *         was successful, None otherwise
+    */
   def apiRequest(endpoint:String,
                  blogUrl:String = "",
                  method:String = "GET",
@@ -178,6 +286,16 @@ class TumblrAPI(apiKey:String, apiSecret:String, oauthToken:String, oauthSecret:
     response.filter(_.isSuccessful).map(_.getBody())
   }
 
+  /** Wraps the apiRequest method for GET requests and parses the response JSON
+    *
+    * @param endpoint the API endpoint to query
+    * @param blogUrl the URL of the blog to get information from. If this is
+    *                a blank string then it's assumed to be your own blog
+    * @param params a ''Map'' of ''Strings'' containing any extra parameters
+    *               to use with the query
+    * @return an ''Option'' class containing a ''TumblrApiResponse'' case
+    *         class if the request was successful, None otherwise
+    */
   def get(endpoint:String,
           blogUrl:String = "",
           params:Map[String,String] = Map.empty[String,String]):Option[TumblrApiResponse] = {
@@ -187,6 +305,17 @@ class TumblrAPI(apiKey:String, apiSecret:String, oauthToken:String, oauthSecret:
     apiResponse.flatMap(TumblrAPI.parseApiGetResponse(_, postType))
   }
 
+  /** Wraps the apiRequest method for POST requests and parses the response JSON
+    *
+    * @param endpoint the API endpoint to query
+    * @param blogUrl the URL of the blog to get information from. If this is
+    *                a blank string then it's assumed to be your own blog
+    * @param params a ''Map'' of ''Strings'' containing any extra parameters
+    *               to use with the query
+    * @param fileData the binary data of the file to be uploaded, if any
+    * @return an ''Option'' class containing a ''String'' if the request
+    *         was successful, None otherwise
+    */
   def post(endpoint:String,
            blogUrl:String = "",
            params:Map[String,String] = Map.empty[String,String],
