bf8e1f6f703049eec90998184d80daa2e817a07d
Guy John <guy@rumblesan.com>
2015-04-22 18:13:47 +0100

    Using newer version of scala

--- project/Build.scala
+++ project/Build.scala
@@ -7,19 +7,17 @@ object UtilsBuild extends Build {
 
     organization := "com.rumblesan.util",
 
-    scalaVersion := "2.10.3",
+    scalaVersion := "2.11.6",
 
-    version := "0.2.0"
+    version := "0.3.0"
 
   )
 
 
-  lazy val specs2 = "org.specs2" %% "specs2" % "2.3.10" % "test"
   lazy val scribe = "org.scribe" % "scribe" % "1.3.2"
   lazy val config = "com.typesafe" % "config" % "1.0.1"
 
   lazy val appDependencies = Seq(
-    libraryDependencies += specs2,
     libraryDependencies += scribe,
     libraryDependencies += config
   )
